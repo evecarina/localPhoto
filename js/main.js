4e4c260d00498b9a968794c7ee7a2b6afb6a299b
carina120794@gmail.com <you@example.com>
2017-09-08 13:47:43 -0500

    el boton limpiar, limpia toda la seccion del contenedor y a la ves limpiar todo el localstorare

--- js/main.js
+++ js/main.js
@@ -1,57 +1,38 @@
 
 //^funcion guardar datos
 $('#guardar').click(function(){
-localStorage.nombre=document.getElementById('clave').value;
-localStorage.comentario=document.getElementById('valor').value;
+    var nombre=document.getElementById('clave').value;
+    var comentario=document.getElementById('valor').value;
+    localStorage.setItem(nombre,comentario);
 
-mostrarDatos();
+    limpiar();
+    recoverData();
 
-limpiar();
 });
-
-//funcion mostrar datos
-function mostrarDatos(){
- var nombre= localStorage.nombre;
- var comentario=localStorage.comentario;
-// var texto= document.createTextNode(localStorage.comentario)
- $("#comentario").append("<div><p>"+ nombre+"</p>"+'<p>'+comentario+'</p></div>');
-
-}
 
 //funcion limpiar datos
 function limpiar(){
-document.getElementById('clave').value="";
-document.getElementById('valor').value="";
+    document.getElementById('clave').value="";
+    document.getElementById('valor').value="";
 }
 // Funcion limpiar lista
 $('#limpiar').click(function(){
-	$("#comentario").empty();
+  	$("#comentario").empty();
+      localStorage.clear();
 
 });
 
 // Funcion recuperar data
-
 function recoverData(){
-for (i=0;i<localStorage.length;i++) { //bucle para recorrer todos los datos
-  var nombre=localStorage.key(i); //obtener la clave del dato
-   var comentario=localStorage.getItem(nombre); //obtener el valor del dato
-
-  $("#comentario").append("<div><p>"+nombre+"</p><p>"+comentario+'</p></div>');
-
-   // var comentario=localStorage.key(i); //obtener la clave del dato
-   // var comentario=localStorage.getItem(comentario);
-   //   $("#comentario").append("<div><p>"+nombre+"</p><p>"+comentario+'</p></div>');
-
+     for(var i=0; i<localStorage.length;i++){
+      var clave=localStorage.key(i);
+      var value=localStorage.getItem(clave);
+     $("#comentario").append('<div><p>'+clave+ '<p>'+value+'</p>'+'</div>');
 
   }
-
 }
-
 
 // Cargar primero todo el contenido de la lista
 $(document).ready(function(){
-    // $("button").click(function(){
-    //     $("p").slideToggle();
-    // });
    recoverData ();
 });
